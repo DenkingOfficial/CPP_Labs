b7759a5f7cf0037e0fd549af576250d13e68854d
Andrei <shershnev_andrei@mail.ru>
2020-05-30 18:02:11 +0500

    Implemented Structure

--- StructureLab/StructureLab.cpp
+++ StructureLab/StructureLab.cpp
@@ -1,6 +1,22 @@
 ﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Car
+{
+	short id;
+	string model;
+	int year;
+};
+
+Car info(short sh, string str, int num)
+{
+	Car car = { sh, str, num };
+	return car;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
